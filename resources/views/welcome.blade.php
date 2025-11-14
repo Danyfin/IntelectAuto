399d4a460f0bffc75eba0fe7e3cf92d12b12da42
RomanStar228 <nastyastar@gmail.com>
2025-11-14 22:36:06 +0500

    bloks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,19 +92,173 @@
 
      <div class="main">
         <div class="background">
-        <div class="main-car">
-            <div class="top-text">
-                <h2 class="text-1">Запчасти для коммерческой техники и спецтехники</h2>
-            </div>
-            <div class="top-down">
-                <p class="text-2">Ваш транспорт — это инструмент, который должен работать, а не стоять в ремонте. Мы помогаем механикам, автопаркам и сервисным центрам держать технику в строю, поставляя качественные запчасти для грузовиков, автобусов, спецтехники и прицепов — точно в срок и по лучшей цене.</p>
-            </div>
-            <div class="top-button">
-                <input class="button-main" type="submit" value="Стать клиентом">
+            <div class="main-car">
+                <div class="top-text">
+                    <h2 class="text-1">Запчасти для коммерческой техники и спецтехники</h2>
+                </div>
+                <div class="top-down">
+                    <p class="text-2">Ваш транспорт — это инструмент, который должен работать, а не стоять в ремонте. Мы помогаем механикам, автопаркам и сервисным центрам держать технику в строю, поставляя качественные запчасти для грузовиков, автобусов, спецтехники и прицепов — точно в срок и по лучшей цене.</p>
+                </div>
+                <div class="top-button">
+                    <input class="button-main" type="submit" value="Стать клиентом">
+                </div>
             </div>
         </div>
+        <div class="info-block">
+            <div class="block">
+                <div class="text-up">Широкий ассортимент</div>
+                <div class="text-down">Запчасти для европейских, российских и китайских марок: от двигателя до подвески.</div>
+            </div>
+            <img class="img-info-block" src="/images/line.svg" alt="">
+
+             <div class="block">
+                <div class="text-up">Проверенные поставщики</div>
+                <div class="text-down">Оригинал и надежные аналоги от ведущих брендов.</div>
+            </div>
+            <img class="img-info-block" src="/images/line.svg" alt="">
+
+             <div class="block">
+                <div class="text-up">Быстрая доставка по РФ</div>
+                <div class="text-down">Работаем с надежными ТК, отправляем в день заказа.</div>
+            </div>
+            <img class="img-info-block" src="/images/line.svg" alt="">
+
+             <div class="block">
+                <div class="text-up">Помощь в подборе</div>
+                <div class="text-down">Наши специалисты помогут найти нужную деталь по VIN, артикулу или фото.</div>
+            </div>
+            <img class="img-info-block" src="/images/line.svg" alt="">
+
+             <div class="block">
+                <div class="text-up">Оплата, как удобно</div>
+                <div class="text-down">Для юрлиц и ИП, безналичный расчет, отсрочка постоянным клиентам.</div>
+            </div>
         </div>
-     </div>
+        <div class="stocks">
+            <div class="theme"><h1>Акции и скидки</h1></div>
+            <div class="blocks">
+                <div class="block1">
+                    <div class="b1-inside">
+                        <div class="b1-info">
+                            <div class="b1-description">
+                                <div class="b1-description-up">Новинка 2025</div>
+                                <div class="b1-description-down">Мега скидка 30% на все фильтры</div>
+                            </div>
+                            <div class="b1-percent">-30%</div>
+                        </div>
+                        <div class="b1-button">
+                            <a class="b1-detailed" href="#"> Подробнее</a>
+                            <img class="b1-img" src="/images/arrow.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="block2">
+                    <div class="b2-inside">
+                        <div class="b2-description">
+                            <div class="b2-text">
+                                <div class="b2-description-up">Мега скидки</div>
+                                <div class="b2-description-down">Бесплатная доставка при заказе от 10 000 ₽</div>
+                            </div>
+                            <div class="b2-img-block">
+                                <img class="b2-img" src="/images/screw.svg" alt="">
+                            </div>
+                        </div>
+                        <div class="b2-button">
+                            <a class="b2-detailed" href="#"> Подробнее</a>
+                            <img class="b2-img" src="/images/arrow.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="block-double">
+                    <div class="block3">
+                        <div class="b3-block">
+                            <div class="b3-info">
+                                <div class="b3-description">
+                                    <div class="b3-description-up">Мега скидки</div>
+                                    <div class="b3-description-down">Сезонное ТО со скидкой</div>
+                                </div>
+                                <div class="b3-button">
+                                    <a class="b3-detailed" href="#"> Подробнее</a>
+                                    <img class="b3-img" src="/images/arrow2.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="b3-block-img">
+                                <img src="/images/tire.svg" alt="" class="b3-img">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block4">
+                        <did class="b4-info">
+                            <div class="b4-button">
+                                <a class="b4-detailed" href="#"> Подробнее</a>
+                                <img class="b4-img" src="/images/arrow.svg" alt="">
+                            </div>
+                            <div class="b4-description">
+                                <div class="b4-description-up">Мега скидки</div>
+                                <div class="b4-description-down">Мега скидки Второй товар — со скидкой 50%</div>
+                            </div>
+                        </did>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="popular">
+            <div class="p-block1">
+                <div class="p-text"><h2>Популярные товары</h2></div>
+                    <div class="arrows">
+                        <img src="/images/arrowL.svg" alt="">
+                        <img src="/images/line2.svg" alt="">
+                        <img src="/images/arrowR.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="p-block2">
+                (сюда доабвить карточки)
+            </div>
+        </div>
+        <div class="forms">
+            <div class="f1-info">
+                <div class="f1-description-up">Работайте с техникой — работайте с профессионалами</div>
+                <div class="f1-description-down">Оставьте заявку или позвоните нам — наши специалисты быстро подберут запчасти под ваш транспорт.</div>
+            </div>
+            <div class="f2-info">
+                <form class="f2-info" action="">
+                   <div class="dropdown">
+                        <button class="dropbtn">Услуги<img class="drop-img"  src="/images/Vector1.svg" alt=""></button>
+                        <div class="dropdown-content">
+                                <a href="#">Услуга 1</a>
+                                <a href="#">Услуга 2</a>
+                                <a href="#">Услуга 3</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">Марка<img class="drop-img" src="/images/Vector1.svg" alt=""></button>
+                        <div class="dropdown-content">
+                                <a href="#">Марка</a>
+                                <a href="#">Марка</a>
+                                <a href="#">Марка</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">Дата и время<img class="drop-img"  src="/images/Vector1.svg" alt=""></button>
+                        <div class="dropdown-content">
+                                <a href="#">Дата и время</a>
+                                <a href="#">Дата и время</a>
+                                <a href="#">Дата и время</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">Филиал<img class="drop-img"  src="/images/Vector1.svg" alt=""></button>
+                        <div class="dropdown-content">
+                                <a href="#">Филиал</a>
+                                <a href="#">Филиал</a>
+                                <a href="#">Филиал</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
         <!-- ФУТЕР НАЧАЛО -->
      <div class="obert">
